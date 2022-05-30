ad7804cc3e5f68be80bf45ad3f80b7ab6823ce4d
skiddieahn <52392658+SkiddieAhn@users.noreply.github.com>
2022-05-30 23:14:14 +0900

    add signup,in,out

---
+++ 안성현/chat/src/main/java/com/websocket/chat/dao/SignMapper.java
@@ -0,0 +1,29 @@
+package com.websocket.chat.dao;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.websocket.chat.dto.chat;
+import com.websocket.chat.dto.user;
+import com.websocket.chat.dto.user_info;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@Mapper
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+public interface SignMapper {
+    void signUp(@Param("user_id") String user_id,
+                @Param("pw") String pw,
+                @Param("user_state") int user_state,
+                @Param("name") String name,
+                @Param("user_dept") String user_dept,
+                @Param("user_sid") String user_sid
+                );
+
+    user getUser(@Param("id") String id);
+    user_info getUserInfo(@Param("id") String id);
+    void setUserState_one(@Param("id") String id);
+    void setUserState_zero(@Param("id") String id);
+}
