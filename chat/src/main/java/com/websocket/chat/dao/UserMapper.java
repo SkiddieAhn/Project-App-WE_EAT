fac109d9f605fff80b0303174fdf8ef9251a6195
LeeSeungMin <712esm@hanmail.com>
2022-06-04 18:45:44 +0900

    edit getprofile

--- chat/src/main/java/com/websocket/chat/dao/UserMapper.java
+++ chat/src/main/java/com/websocket/chat/dao/UserMapper.java
@@ -1,11 +1,14 @@
 package com.websocket.chat.dao;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.websocket.chat.dto.user_info;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @Mapper
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public interface UserMapper {
+    user_info getProfile(@Param("user_id") String user_id);
 }
--- chat/src/main/java/com/websocket/chat/service/UserService.java
+++ chat/src/main/java/com/websocket/chat/service/UserService.java
@@ -1,4 +1,25 @@
 package com.websocket.chat.service;
 
-public class UserService {
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.websocket.chat.dao.UserMapper;
+import com.websocket.chat.dto.user_info;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+interface UserServiceIF{
+    public user_info getProfile(String user_id);
+}
+@Slf4j
+@RequiredArgsConstructor
+@Service
+@Getter
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+public class UserService implements UserServiceIF{
+    private final UserMapper userMapper;
+    @Override
+    public user_info getProfile(String user_id) {
+        userMapper.getProfile(user_id);
+    }
 }
